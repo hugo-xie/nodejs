b9037b880583946c5dcfb5a1daaf318d21b53474
hugo-xie <m15955038579@163.com>
2016-09-26 14:20:39 +0800

    http

--- http.js
+++ http.js
@@ -2,11 +2,20 @@
 var http = require('http');
 var util = require('util');
 var  fs = require('fs');
-http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/plain'});
-  // var rs = fs.createReadStream('1.mp4');
-  res.write(req.method);
-  res.write(util.inspect(req.headers));
-  // rs.pipe(res);
-  res.end();
-}).listen(4000);
+// http.createServer(function(req,res){
+//   res.writeHead(200,{'Content-Type':'text/plain'});
+//   // var rs = fs.createReadStream('1.mp4');
+//   res.write(req.method);
+//   res.write(util.inspect(req.headers));
+//   // rs.pipe(res);
+//   res.end();
+// }).listen(4000);
+var options = {
+  host:"www.baidu.com",
+  port:80,
+  path:"/index.html"
+};
+
+http.get(options,function(res){
+  console.log('Got response:'+res.statusCode);
+});
